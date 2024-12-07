2161503820d16229b48d913dd133fe8eba607e8f
Sergey Mikhno <sergey.mikhno@galenica.com>
2024-12-07 09:50:31 +0100

    0.0.4

--- setup.py
+++ setup.py
@@ -1,29 +1,14 @@
 import numpy as np
-import setuptools
+from distutils.core import setup, Extension
 
-rlsa_module = setuptools.Extension("rlsa", sources=["rlsa/rlsa.c"], include_dirs=[np.get_include()])
+rlsa_module = Extension("rlsa", sources=["rlsa/rlsa.c"], include_dirs=[np.get_include()])
 
 
-setuptools.setup(
+setup(
     name="rlsa",
-    version="0.0.2",
+    version="0.0.4",
     description="Run Length Smoothing Algorithm",
-    long_description=open("README.md", encoding="utf-8").read(),
-    long_description_content_type='text/markdown',
-    url="https://github.com/hoel-bagard/rlsa",
-    author="Hoel Bagard",
-    classifiers=[
-        "License :: OSI Approved :: MIT License",
-        "Programming Language :: Python :: 3",
-        "Programming Language :: Python :: 3.8",
-        "Programming Language :: Python :: 3.9",
-        "Programming Language :: Python :: 3.10"
-    ],
-    keywords="rlsa",
-    zip_safe=False,
     install_requires=["numpy"],
     ext_modules=[rlsa_module],
     packages=["rlsa"],
-    include_package_data=True,
-    python_requires=">=3.8",
 )
